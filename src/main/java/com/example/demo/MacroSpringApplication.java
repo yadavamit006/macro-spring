197194b3eb5c526291e959fdf461c9f6ae3266f8
yadavamit006 <yadavamit006@gmail.com>
2022-01-04 12:50:54 +0530

    initial commit

---
+++ src/main/java/com/example/demo/MacroSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacroSpringApplication.class, args);
+	}
+
+}
