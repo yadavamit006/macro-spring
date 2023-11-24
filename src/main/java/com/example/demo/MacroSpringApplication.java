1cb7ab4aa3a280b8c3890d8df0169ecf1dd1519c
Amit Yadav <yadavamit006@gmail.com>
2023-11-24 16:59:16 +0530

    Removed unnecessary changes

--- src/main/java/com/example/demo/MacroSpringApplication.java
+++ src/main/java/com/example/demo/MacroSpringApplication.java
@@ -6,13 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MacroSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MacroSpringApplication.class, args);
-		System.out.println("sdfed");
-		System.out.println("Backend commit");
-		// Check for comments
-		//Test added comment
-		// Comments by Feature Amdoc
-		//Develop has made changes
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MacroSpringApplication.class, args);
+    }
 }
