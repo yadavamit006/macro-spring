7a8270cb82fb2adc715357fd979913839e464c9a
Amit Yadav <yadavamit006@gmail.com>
2023-12-06 16:45:08 +0530

    Update MacroSpringApplication.java

--- src/main/java/com/example/demo/MacroSpringApplication.java
+++ src/main/java/com/example/demo/MacroSpringApplication.java
@@ -1,13 +1,9 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class MacroSpringApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MacroSpringApplication.class, args);
     }
-	//All redendent code are removed
 }
